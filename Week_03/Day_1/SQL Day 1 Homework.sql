1e113e2bf872d488d37135d81c246755ff6de5cb
malcheyne <92717988+malcheyne@users.noreply.github.com>
2021-11-15 23:06:04 +0000

    Finished
    Q17 needs work

--- Week_03/Day_1/SQL Day 1 Homework.sql
+++ Week_03/Day_1/SQL Day 1 Homework.sql
@@ -151,10 +151,106 @@ WHERE pension_enrol IS TRUE AND
  * those employees in the ‘Engineering’ department who 
  * work 1.0 full-time equivalent hours (fte_hours)?*/
 
+SELECT 
+	salary,
+	fte_hours
+FROM employees 
+WHERE department = 'Engineering' AND fte_hours >= 1.0;
+
+SELECT 
+	salary
+FROM employees 
+WHERE department = 'Engineering' AND fte_hours >= 1.0
+LIMIT 1;
+
+/* MVP */
+/* Q15 */
+/* Return a table containing each employees first_name, 
+ * last_name, full-time equivalent hours (fte_hours), salary, 
+ * and a new column effective_yearly_salary which should 
+ * contain fte_hours multiplied by salary.*/
+
+SELECT 
+	first_name, 
+ 	last_name,
+ 	fte_hours,
+ 	salary, 
+ 	(fte_hours*salary) AS effective_yearly_salary
+ FROM employees;
+
+/* EXT */
+/* Q16 */
+/* The corporation wants to make name badges for a forthcoming conference. 
+ * Return a column badge_label showing employees’ first_name and last_name 
+ * joined together with their department in the following style: ‘Bob Smith - Legal’. 
+ * Restrict output to only those employees with stored first_name, last_name and department. 
+ */
+
+SELECT  
+  first_name, 
+  last_name, 
+  department,
+  CONCAT(first_name, ' ', last_name, ' ', '-', ' ', department) AS badge_label
+FROM employees
+WHERE (first_name, last_name, department) IS NOT NULL;
+
+/* EXT */
+/* Q17 */
+/* One of the conference organisers thinks it would be nice to add the year of the 
+ employees’ start_date to the badge_label to celebrate long-standing colleagues, in 
+ the following style ‘Bob Smith - Legal (joined 1998)’. Further restrict output to only 
+ those employees with a stored start_date.
+
+[If you’re really keen - try adding the month as a string: ‘Bob Smith - Legal (joined July 1998)’] */
+
+SELECT  
+  first_name, 
+  last_name, 
+  department,
+  start_date,
+  CONCAT(first_name, ' ', last_name, ' ', '-', ' ', department, ' ', '(', 'joined', ' ', start_date, ')') AS badge_label
+FROM employees
+WHERE (first_name, last_name, department, start_date) IS NOT NULL;
 
 
 
+SELECT  
+  first_name, 
+  last_name, 
+  department,
+  format(start_date, 'MMM-yy') AS month_year, 
+  CONCAT(first_name, ' ', last_name, ' ', '-', ' ', department, ' ', '(', 'joined', ' ', month_year,')') AS badge_label
+FROM employees
+WHERE (first_name, last_name, department, start_date) IS NOT NULL;
+
+/* EXT */
+/* Q18 */
+/* Return the first_name, last_name and salary of all employees together 
+ * with a new column called salary_class with a value 'low' where salary is less 
+ * than 40,000 and value 'high' where salary is greater than or equal to 40,000. */
+
+SELECT 
+	first_name, 
+  	last_name,
+  	salary,
+  		CASE WHEN salary IS NULL THEN 'Unknown'
+  		WHEN salary >= 40000 THEN 'high'
+  					ELSE 'low' END AS salary_class		
+FROM employees;
 
 
 
+/* https://www.sqlshack.com/understanding-sql-server-case-statement/
+ * You can evaluate multiple conditions in the CASE statement.
 
+Let’s write a SQL Server CASE statement which sets the value of the condition column to “New” if the value in the model column is greater than 2010, to ‘Average’ if the value in the model column is greater than 2000, and to ‘Old’ if the value in the model column is greater than 1990.
+
+Look at the following script:*/
+
+SELECT name,
+       model,
+       CASE WHEN model > 2010 THEN 'New'
+      WHEN model > 2000 THEN 'Average'
+      WHEN model > 1990 THEN 'Old'
+                ELSE 'Old' END AS condition
+  FROM Cars
